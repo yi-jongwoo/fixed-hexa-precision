140fc3b5b87895302d75b3260a31a69a7ac354e3
yi-jongwoo <82140757+yi-jongwoo@users.noreply.github.com>
2023-05-09 22:34:29 +0900

    Update fixedprec.c

--- fixedprec.c
+++ fixedprec.c
@@ -22,7 +22,7 @@ void m_plus(ui* r,ui* x,ui* y){
 	}
 }
 ui m_minus(ui* r,ui* x,ui* y){
-	long long k=0;
+	signed long long k=0;
 	for(int i=0;i<NM;i++){
 		k+=(ull)x[i]-y[i];
 		r[i]=k;
@@ -40,7 +40,7 @@ void m_times(ui* r,ui* x,ui* y){
 	for(int i=0;i<NM;i++)
 		r[i]=tmp[i+N];
 }
-void m_div(ui* r,ui* x,ui* y){
+void m_div(ui* r,ui* x,ui* y){ // *r <-> *y
 	ui t[NM*2]={0};
 	for(int i=0;i<NM;i++)
 		r[i]=0;
@@ -52,19 +52,9 @@ void m_div(ui* r,ui* x,ui* y){
 			if(j)
 				t[j]|=t[j-1]>>31;
 		}
-		/*
-		std::cout<<'T';
-		for(int k=NM*2-1;k>=0;k--)
-			std::cout<<t[k]<<' ';std::cout<<std::endl;
-		std::cout<<'K';
-		for(int k=NM-1;k>=0;k--)
-			std::cout<<y[k]<<' ';std::cout<<std::endl;
-		*/
 		for(int j=NM*2-1;j>=0;j--){
 			ui yy=j>=N&&j<NM+N?y[j-N]:0;
 			if(t[j]>yy){
-				//std::cout<<j<<' '<<i<<std::endl;
-				
 				r[i/32]|=1u<<(i%32);
 				if(m_minus(t+N,t+N,y))
 					for(int k=NM+N;!t[k]--;k++);
@@ -75,14 +65,52 @@ void m_div(ui* r,ui* x,ui* y){
 		}
 	}
 }
-void m_log2(ui* r){
-	
+void m_log2(ui* r,ui* x){
+	ui t[6];
+	int kk=0;
+	for(int i=0;i<6;i++)
+		t[i]=x[i];
+	while(t[5]||t[4]>1){
+		//std::cout<<r[5]<<std::endl;
+		kk++;
+		for(int j=0;j<NM;j++){
+			t[j]>>=1;
+			if(j!=NM-1)
+				t[j]|=t[j+1]<<31;
+		}
+	}
+	while(!t[4]){
+		kk--;
+		for(int j=NM-1;j>=0;j--){
+			t[j]<<=1;
+			if(j)
+				t[j]|=t[j-1]>>31;
+		}
+	}
+	if(kk>=0)
+		r[5]=0;
+	else
+		r[5]=-1;
+	r[4]=kk;
+	r[3]=r[2]=r[1]=r[0]=0;
+	for(int i=N*32-1;i>=0;i--){
+		m_times(t,t,t);
+		if(t[4]>1){
+			std::cout<<i<<std::endl;
+			r[i/32]|=1u<<(i%32);
+			for(int j=0;j<NM;j++){
+				t[j]>>=1;
+				if(j<NM-1)
+					t[j]|=t[j+1]<<31;
+			}
+		}
+	}
 }
 
 int main(){
 	ui a[6]={0,0,0,0,134,0};
 	ui b[6]={0,0,0,0,8,0};
 	ui c[6],d[6];
-	m_div(c,a,b);
-	std::cout<<c[4];
+	m_log2(c,a);
+	std::cout<<c[3];
 }
